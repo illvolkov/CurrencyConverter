89736c6411f5392e121e02df9d679eea7c2c383f
Ilya Volkov <illvolkov@bk.ru>
2022-08-23 16:46:00 +0500

    Project preparation

---
+++ CurrencyConverter/Sources/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  CurrencyConverter
+//
+//  Created by Ilya Volkov on 23.08.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
